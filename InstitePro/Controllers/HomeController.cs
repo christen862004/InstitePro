1b33b6663d35f24874f99501342aa3122949e2c0
Christen <christen.foad@gmail.com>
2024-03-09 13:54:43 +0200

    Add project files.

---
+++ InstitePro/Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+using InstitePro.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace InstitePro.Controllers
+{
+    //public 
+    //inherit from Contoller
+    //End With Controller
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //url/Home/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //url:/Home/privacy
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //MEthod ==> Action
+        //Action must be public 
+        //Action cant be static
+        //Action cant be OverLoad
+        //Home/ShowMsg
+        //public string ShowMsg()
+        //{
+        //    return "Hello";
+        //}
+        public ViewResult ShowView()
+        {
+            //Logic
+            //DEclare
+            ViewResult result = new ViewResult();
+            //Set
+            result.ViewName = "View1";
+            //Retrun
+            return result;
+        }
+        public ContentResult ShowMsg()
+        {
+            //logic
+            //DEclare
+            ContentResult result = new ContentResult();
+            //set
+            result.Content = "Heelo from Action";
+            //erturn
+            return result;
+        }
+        //---------------------------------------
+        //Home/ShowMix?stdno=1&name=ahmed
+        public ActionResult ShowMix(int stdNo)//,string name)
+        {
+            if (stdNo == 13)
+            {
+                return Content("HEllo");
+            }
+            else
+            {
+                return View("View1");
+            }
+        }
+
+
+        public ViewResult showview2()
+        {
+            //logic
+            return View("View2");
+        }
+        //ViewResult View(string viewName)
+        //{
+        //    ViewResult result = new ViewResult();
+        //    //Set
+        //    result.ViewName = viewName;
+        //    //Retrun
+        //    return result;
+        //}
+
+
+
+
+
+        //Action CAn REturn
+        /*
+         1)String  --> ContentResult  ==>Content
+         2)View    --> ViewResult     ==> View
+         3)Json    --> JsonREsult     ==>json
+         4)File    --> fileREsult
+         5)NotFound -> NotFoundResult
+         6)Unauthor -> UnauthotizeResult
+         ......
+         */
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
