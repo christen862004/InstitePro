ba6ae780db4d959db8bac8affed76e7eeaa09502
Christen <christen.foad@gmail.com>
2024-03-17 13:10:52 +0200

    Day6 update

--- InstitePro/Controllers/DepartmentController.cs
+++ InstitePro/Controllers/DepartmentController.cs
@@ -4,12 +4,40 @@ namespace InstitePro.Controllers
 {
     public class DepartmentController : Controller
     {
+        //Department/GetEmpByDEptID?deptID=1
+        public IActionResult GetEmpByDEptID(int deptID)
+        {
+            var  employees = context.Employee
+                .Where(e => e.DepartmentId == deptID)
+                .Select(e => new { e.Name,e.Id}).ToList();
+            return Json(employees);
+        }
+        public IActionResult ShowDeptsEmp()
+        {
+            List<Department> depts = context.Department.ToList();
+            return View("ShowDeptsEmp", depts);//List<DEpartment>
+        }
+
+
+
         //action by default can handel any reuest type(Get |Post)
         ITIContext context=new ITIContext();
         public DepartmentController()
         {
             
         }
+
+        public IActionResult GetPartialCard(int id)
+        {
+            Department deptModel=context.Department.FirstOrDefault(x => x.Id == id);
+            // return PartialView("_DeptCard",deptModel);
+            return Json(deptModel);
+        }
+
+
+
+
+
         public IActionResult Index()
         {
             List<Department> deptListModel = context.Department.ToList();
@@ -38,6 +66,16 @@ namespace InstitePro.Controllers
                                                     //return RedirectToAction("DEtails", "Employee", new { id = 1 });
             }
             return View("NewDept", Dept);//view=NewDept ,Model= DEpartment
+        }
+
+
+        public IActionResult DEtails(int id)
+        {
+            return View("Details", context.Department.FirstOrDefault(d => d.Id == id));
+        }
+        public IActionResult Delete(int id)
+        {
+            return View("Delete", context.Department.FirstOrDefault(d => d.Id == id));
         }
     }
 }
